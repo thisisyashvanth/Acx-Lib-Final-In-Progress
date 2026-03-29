3980e74829b2520446394f73d37f04b4aab5c0f3
Yashvanth <yashvanth.vijayabalaji@acxhange.com>
2026-03-29 18:39:13 +0530

    DB Config Created

---
+++ backend/core/database.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+engine = create_engine(url=DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
