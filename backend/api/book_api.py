7ae25b565e6c87acfe19e81fa6baedff61651414
Yashvanth <yashvanth.vijayabalaji@acxhange.com>
2026-03-29 20:08:37 +0530

    Restricted Authenticated Only Access in book_api.py

--- backend/api/book_api.py
+++ backend/api/book_api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from schemas.book_schema import CreateBookResp, CreateBookReq, DeleteBookResp, GetBookResp
 from sqlalchemy.orm import Session
 from core.database import get_db
-from security.dependency import require_hr
+from security.dependency import get_current_user, require_hr
 from services import book_service
 
 
@@ -15,12 +15,12 @@ def create_book(book: CreateBookReq, db: Session = Depends(get_db), user=Depends
 
     
 @router.get("/get-all", response_model=list[GetBookResp])
-def get_all_books(db: Session = Depends(get_db)):
+def get_all_books(db: Session = Depends(get_db), user = Depends(get_current_user)):
     return book_service.get_all_books(db)
 
 
 @router.get("/get/{id}", response_model=GetBookResp)
-def get_book(id: int, db: Session = Depends(get_db)):
+def get_book(id: int, db: Session = Depends(get_db), user = Depends(get_current_user)):
     return book_service.get_book(id, db)
 
 
