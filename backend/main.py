410dfa1292c850e6f78468a1a18172bf89453b34
Yashvanth <yashvanth.vijayabalaji@acxhange.com>
2026-04-05 10:23:13 +0530

    main.py Modified for Clearer Understanding

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,22 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from core.database import Base, engine
-
+from core.database import engine, Base 
 from api.user_api import router as user_router
 from api.auth_api import router as auth_router
 from api.book_api import router as book_router
 from api.request_api import router as request_router
-
 from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
+# Un comment on 1st Time Starting App in Local [Remove in Cloud]
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     Base.metadata.create_all(bind=engine)
+#     yield
 
-app = FastAPI(title="Acxhange Library System", lifespan=lifespan)
-
-app.include_router(user_router)
-app.include_router(auth_router)
-app.include_router(book_router)
-app.include_router(request_router)
+app = FastAPI(title="Acxhange Library System")
+# app = FastAPI(title="Acxhange Library System", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -29,3 +25,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(user_router)
+app.include_router(auth_router)
+app.include_router(book_router)
+app.include_router(request_router)
+
+
+# Only onetime when deploying to RDS
+# @app.get("/setup")
+# def setup():
+#     from core.database import engine, Base
+#     Base.metadata.create_all(bind=engine)
+#     return {"message": "Tables created"}
+
+
+handler = Mangum(app)
